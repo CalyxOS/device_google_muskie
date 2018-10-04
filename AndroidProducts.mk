6d208210ac9585101555f05e6792558224557653
Chirayu Desai <chirayudesai1@gmail.com>
2018-10-04 16:42:25 +0530

    Add Calyx product makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,7 +17,9 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_walleye.mk \
     $(LOCAL_DIR)/aosp_walleye_test.mk \
+    $(LOCAL_DIR)/calyx_walleye.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_walleye-userdebug \
-    aosp_walleye_test-userdebug
+    aosp_walleye_test-userdebug \
+    calyx_walleye-userdebug
