da34aef1828e712b5b886105f000102c5ca964c1
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-12 02:17:42 +0530

    Inherit device-calyx.mk
    Change-Id: I9b5d0e58cdac0e5eba5f6a498775ad8072df37a3

--- calyx_walleye.mk
+++ calyx_walleye.mk
@@ -1,6 +1,8 @@
 # Inherit some common stuff.
 $(call inherit-product, vendor/calyx/config/common.mk)
 
+$(call inherit-product, device/google/wahoo/device-calyx.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/google/muskie/aosp_walleye.mk)
 
