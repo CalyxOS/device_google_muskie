640bbcdd13bf51a50e73a62d8694dc28ab16ccd6
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: I54d89dfe2768fe60df882273944cda618218a89a

--- calyx_walleye.mk
+++ calyx_walleye.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/calyx/config/common.mk)
 $(call inherit-product, device/google/wahoo/device-calyx.mk)
 $(call inherit-product, device/google/muskie/aosp_walleye.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/walleye/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_walleye
 PRODUCT_MODEL := Pixel 2
